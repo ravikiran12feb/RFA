bad303391673ddd6db37f21295fef466332650b1
RM00363966 <RM00363966@techMahindra.com>
2017-04-13 19:47:39 +0530

    r-57

--- RTA/src/rfff/Remteam.java
+++ RTA/src/rfff/Remteam.java
@@ -15,7 +15,7 @@ public class Remteam {
 		System.out.println("akshata-3");
 		System.out.println("Ekta-1");
 		System.out.println("Ravi-5");
-		System.out.println("Ravi-6");
+		System.out.println("Ravi-61");
 	}
 
 }
